d5cec069f956d8e8dc5d5bede129cb961ee965ff
zlouvat <zacklouvat@gmail.com>
2025-02-04 07:58:56 -0500

    Added functionality for disappearing buttons, doesn't currently work but will fix in future

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,49 @@
-import ListGroup from "./components/ListGroup";
 import Button from "./components/Button";
 import Card from "./components/Card";
+import { useState } from "react";
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+  const [cssVisible, setCSSVisibility] = useState(false);
+  const [javaScriptVisible, setJavaScriptVisibility] = useState(false);
+  const [htmlVisible, setHTMLVisibility] = useState(false);
+  const [gitVisible, setGitVisibility] = useState(false);
+
   return (
     <div>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
+      <Button color="primary" onClick={() => setCSSVisibility(true)}>
         CSS
       </Button>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
+      <Button color="primary" onClick={() => setJavaScriptVisibility(true)}>
         JavaScript
       </Button>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
+      <Button color="primary" onClick={() => setHTMLVisibility(true)}>
         HTML
       </Button>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
+      <Button color="primary" onClick={() => setGitVisibility(true)}>
         GitHub
       </Button>
-      <Card title="CSS">Info on CSS</Card>
-      <Card title="JavaScript">Info on CSS</Card>
-      <Card title="HTML">Info on CSS</Card>
-      <Card title="Github">Info on CSS</Card>
+      {cssVisible && (
+        <Card onClose={() => setCSSVisibility(false)} title="CSS">
+          Info on CSS
+        </Card>
+      )}
+      {javaScriptVisible && (
+        <Card onClose={() => setJavaScriptVisibility(false)} title="JavaScript">
+          Info on JavaScript
+        </Card>
+      )}
+      {htmlVisible && (
+        <Card onClose={() => setHTMLVisibility(false)} title="HTML">
+          Info on HTML
+        </Card>
+      )}
+      {gitVisible && (
+        <Card onClose={() => setGitVisibility(false)} title="GitHub">
+          Info on GitHub
+        </Card>
+      )}
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,15 +2,24 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
-  onClick: () => void;
+  onClose: () => void;
   title: string;
 }
 
-const Card = ({ children, onClick, title }: Props) => {
+const Card = ({ children, onClose, title }: Props) => {
   return (
-    <div className="card" onClick={onClick}>
+    <div className="card">
       <div className="card-body">
-        <h2 className="card-title">{title}</h2>
+        <h2 className="card-title">
+          {title}
+          <button
+            type="button"
+            onClick={onClose}
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </h2>
         <p className="card-text">{children}</p>
       </div>
     </div>
